c5ff0a4cf66f911637dd7dbb9dd0f0e7e9c27ead
Luís António da Rosa Neng <luisarn@gmail.com>
2025-11-23 00:16:07 +0800

    Update funasr_http_client.py

--- funasr_http_client.py
+++ funasr_http_client.py
@@ -5,7 +5,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--host", type=str, default="127.0.0.1", required=False, help="sever ip")
-parser.add_argument("--port", type=int, default=8000, required=False, help="server port")
+parser.add_argument("--port", type=int, default=8200, required=False, help="server port")
 parser.add_argument(
     "--audio_path", type=str, default="asr_example_zh.wav", required=False, help="use audio path"
 )
